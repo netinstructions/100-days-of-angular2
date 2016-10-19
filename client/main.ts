f703859386320e3e0a84501f36bf7c45bc7c90b3
Stephen <stephen@netinstructions.com>
2016-10-19 16:37:44 -0700

    day 6 - check to enableProdMode

--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
 
 const platform = platformBrowserDynamic();
+
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
 
 platform.bootstrapModule(AppModule);
