44a560f2d695ddf8de893db486c4e5fb61e9de57
Stephen <stephen@netinstructions.com>
2016-11-10 16:59:36 -0800

    day 9 - use TypeScript on back end

---
+++ server/app.ts
@@ -0,0 +1,57 @@
+import * as express from 'express';
+import * as expressSanitizer from 'express-sanitizer';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+
+var api = require('./routes/api');
+
+var app: express.Application = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressSanitizer());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/api', api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err : any = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
