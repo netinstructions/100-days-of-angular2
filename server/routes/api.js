90f4ba507ac76637797d11c51065a505051a33a8
Stephen <stephen@netinstructions.com>
2016-11-14 14:40:24 -0800

    day 9 - use TypeScript on back end

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var sanitizer = require('sanitizer');
 var router = express.Router();
 
-let messageOfTheMoment = "If you ever fall off the Sears Tower, just go real limp, because maybe you'll look like a dummy and people will try to catch you because, hey, free dummy.";
+var messageOfTheMoment = "If you ever fall off the Sears Tower, just go real limp, because maybe you'll look like a dummy and people will try to catch you because, hey, free dummy.";
 
 router.get('/test', function(req, res, next) {
   res.json({'testMessage': 'everything is going to be ok', 'someNumber': 457});
@@ -14,8 +14,8 @@ router.get('/get-message-of-the-moment', function(req, res, next) {
 
 router.post('/post-message', function(req, res, next) {
   if(req.body.message && req.body.message.length < 500) {
-    let unsafeMessage = req.body.message;
-    let sanitizedMessage = sanitizer.sanitize(req.body.message);
+    var unsafeMessage = req.body.message;
+    var sanitizedMessage = sanitizer.sanitize(req.body.message);
     console.log('New message of the moment (unsafe): ' + unsafeMessage);
     console.log('New message of the moment (clean) : ' + sanitizedMessage);
     messageOfTheMoment = sanitizedMessage;
